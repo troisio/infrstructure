e51683d28826ea57bb951d82f373bf35a374663c
Luis E. Banegas Saybe <luis@trois.io>
2017-01-08 13:23:47 -0500

    feat(ssh): Install generates ssh key

--- centos7/install.sh
+++ centos7/install.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-#Install boilerplate
+ssh-keygen -b 4048 -f ~/.ssh/id_rsa -t rsa -N "" -C $1
+
 yum -y update
 yum install -y git-all nano man man-pages curl
 yum clean all
 
 #https://docs.docker.com/engine/installation/linux/centos/#install-with-the-script
-#Install docker
 
 curl -fsSL https://get.docker.com/ | sh
 sudo systemctl enable docker.service
 sudo systemctl start docker
-
-#Test docker
-sudo docker run --rm hello-world
-docker rmi hello-world
