9f37e4342ed113e33fa0d65d21cecd0e60e16309
Luis E. Banegas Saybe <luis@trois.io>
2017-05-17 22:46:22 -0400

    feat(gcp): GCP credentials uploaded to image

--- jivecakeapi/install.sh
+++ jivecakeapi/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp www_jivecake_com.jks settings.yml docker-compose.yml $1:~
+scp gcp-credentials.json www_jivecake_com.jks settings.yml docker-compose.yml $1:~
 scp id_rsa id_rsa.pub $1:~/.ssh
 ssh $1 "chmod 600 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub"
 ssh $1 'bash -s' < install-dependencies.sh
